40985937fe5de1fad4460eb940ebe23650d102c3
yuuta-git12 <tayu2201@icloud.com>
2024-12-04 22:56:52 +0900

    UserCardのorganism作成 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { PrimaryButton } from './components/atoms/button/PrimaryButton'
 import { SecondaryButton } from './components/atoms/button/SecondaryButton'
 import { SearchInput } from './components/molecules/SearchInput'
+import { UserCard  } from './components/organisms/user/UserCard'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <SecondaryButton>検索</SecondaryButton>
             
         <SearchInput />
+        <UserCard />
       </div>
     </>
   )
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+    return (
+        <div>
+            <imag alt="プロフィール" />
+            <p>名前</p>
+            <dl>
+                <dt>メール</dt>
+                <dd>1111@aaaa.com</dd>
+                <dt>TEL</dt>
+                <dd>000-9999-8888</dd>
+                <dt>会社名</dt>
+                <dd>AAAA会社</dd>
+                <dt>WEB</dt>
+                <dd>http://ddded.com</dd>
+            </dl>
+        </div>
+    )
+}
