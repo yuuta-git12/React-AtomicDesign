fc14a672217394a1b9d5a8329a7ad6b7ce93076d
yuuta-git12 <tayu2201@icloud.com>
2024-12-04 22:49:48 +0900

    検索フォームと検索ボタンのMolecule作成

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { PrimaryButton } from './components/atoms/button/PrimaryButton'
 import { SecondaryButton } from './components/atoms/button/SecondaryButton'
+import { SearchInput } from './components/molecules/SearchInput'
 
 function App() {
   return (
     <>
       <div className='App'>
         <PrimaryButton>テスト</PrimaryButton>
-        <br />
-        <br />
         <SecondaryButton>検索</SecondaryButton>
+            
+        <SearchInput />
       </div>
     </>
   )
