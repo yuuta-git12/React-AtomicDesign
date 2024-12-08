7f36a412df175068f6f7b4eb0c7926834bf7b669
yuuta-git12 <tayu2201@icloud.com>
2024-12-08 22:20:20 +0900

    Organismの作成２

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import { SecondaryButton } from './components/atoms/button/SecondaryButton'
 import { SearchInput } from './components/molecules/SearchInput'
 import { UserCard  } from './components/organisms/user/UserCard'
 
+const user = {
+  name: "ユータ",
+  image: "https://images.unsplash.com/photo-1585910166082-b505ca55cd14?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  email: "1234@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "http://google.com"
+}
+
 function App() {
   return (
     <>
@@ -15,7 +26,7 @@ function App() {
         <SecondaryButton>検索</SecondaryButton>
             
         <SearchInput />
-        <UserCard />
+        <UserCard user = {user} />
       </div>
     </>
   )
--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,18 +1,35 @@
-export const UserCard = () => {
+import styled from "styled-components"
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+    const { user } = props;
     return (
-        <div>
-            <imag alt="プロフィール" />
-            <p>名前</p>
-            <dl>
+        <Card>
+            <UserIconWithName image={user.image} name={user.name}/>
+            <SDl>
                 <dt>メール</dt>
-                <dd>1111@aaaa.com</dd>
+                <dd>{user.email}</dd>
                 <dt>TEL</dt>
-                <dd>000-9999-8888</dd>
+                <dd>{user.phone}</dd>
                 <dt>会社名</dt>
-                <dd>AAAA会社</dd>
+                <dd>{user.company.name}</dd>
                 <dt>WEB</dt>
-                <dd>http://ddded.com</dd>
-            </dl>
-        </div>
+                <dd>{user.website}</dd>
+            </SDl>
+        </Card>
     )
 }
+
+const SDl = styled.dl`
+    text-align: ;
+    margin-bottom: 0px;
+    dt {
+        float: left;
+    }
+    dd {
+        padding-left: 32px;
+        padding-bottom: 8px;
+        overflow-wrap: break-word;
+    }
+`
